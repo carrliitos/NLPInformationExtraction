7f62dca86bb9d4357136af68771e9c0147a34349
carrliitos <salazar.ten80@gmail.com>
2020-06-15 19:40:01 -0500

    added PDFManager

---
+++ src/main/preprocessing/textCollection/ExtractText.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.io.IOException;
+
+public class ExtractText {
+	public static void main(String[] args) {
+		String path = "/home/carlitos/Documents/Projects/git/NLPInformationExtraction/src/main/preprocessing/textCollection";
+		try {
+			// String files; 
+			// File folder = new File(path);
+		}
+	}
+}
--- src/samplePDFBox/PDFInfo.java
+++ src/samplePDFBox/PDFInfo.java
@@ -3,34 +3,63 @@
 * Sample use of PDFBox for extracting information out of PDFs
 */
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.File;
+import java.io.FileInputStream;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDDocumentInformation;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.pdfparser.PDFParser;
+import org.apache.pdfbox.cos.COSDocument;
 
 public class PDFInfo {
 	public static void main(String[] args) {
 		String path = "/home/carlitos/Documents/Research/MedicalIntent/CaseReports/CamelBite.pdf";
-		File file = new File(path);
+		// File file = new File(path);
+		PDDocument pdd = null;
 
-		try{
-			// Load the pdf file
-			PDDocument document = new PDDocument();
-			document = PDDocument.load(file);
+		// try{
+		// 	// Load the pdf file
+		// 	// PDDocument document = new PDDocument();
+		// 	// document = PDDocument.load(file);
 
-			// Get the PDDocumentInformation object from the PDDocument created
-			PDDocumentInformation information = document.getDocumentInformation();
+		// 	// Parse and read the document
+		// 	PDFParser parser = new PDFParser(inputStream);
+		// 	parser.parse();
+		// 	COSDocument cosDoc = parser.getDocument();
+		// 	PDDocument pdDoc = new PDDocument(cosDoc);
 
-			String title = information.getTitle();
-			String author = information.getAuthor();
-			String subject = information.getSubject();
-			String producer = information.getProducer();
-			String trapped = information.getTrapped();
+		// 	// Get the PDDocumentInformation object from the PDDocument created
+		// 	PDDocumentInformation information = document.getDocumentInformation();
+
+		// 	// PDFTextStripper
+		// 	PDFTextStripper stripper = new PDFTextStripper(cosDoc);
+
+		// 	String title = information.getTitle();
+		// 	String author = information.getAuthor();
+		// 	String subject = information.getSubject();
+		// 	String producer = information.getProducer();
+		// 	String trapped = information.getTrapped();
+		// 	String keywords = information.getKeywords();
+
+		// 	text = stripper.getText(pdDoc);
 			
-			System.out.printf("Title: %s%nAuthor: %s%nSubject: %s%nProducer: %s%nTrapped: %s%n", title, author, subject, producer,trapped);
+		// 	// System.out.printf("Title: %s%nAuthor: %s%nSubject: %s%nProducer: %s%nTrapped: %s%n", title, author, subject, producer,trapped);
+		// 	// System.out.printf("Keywords: %s%n", keywords);
+		// 	System.out.println("Text: " + text);
+		try{
+			FileInputStream inputStream = new FileInputStream(path);
+			pdd = PDDocument.load(inputStream);
+			PDFTextStripper textStripper = new PDFTextStripper();
+			// String pageStart = textStripper.getPageStart();
+			// System.out.printf("Start: %s%n", pageStart);
+			// System.out.println("Paragraph end: " + textStripper.getParagraphEnd());
+			System.out.println("Text of Document: " + textStripper.getText(pdd));
+
+			inputStream.close();
 		}catch(IOException e) {
 			System.out.println("IOException on PDFInfo sample");
-			return;
 		}
 	}
 }
