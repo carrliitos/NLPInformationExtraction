347a7162bd00e684234865f78427a4bb9c32ded1
carrliitos <salazar.ten80@gmail.com>
2020-06-16 23:18:36 -0500

    writeToNewFile.java update

--- src/samplePDFBox/writeToNewFile.java
+++ src/samplePDFBox/writeToNewFile.java
@@ -1,16 +1,17 @@
 import java.io.File;
 import java.io.FileWriter;
-import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.IOException;
 
+/**Sample class file for testing
+*/
 public class writeToNewFile {
 	public static void main(String[] args) {
 		try {
 			// Use a scanner for all input files
 			String dir = "/home/carlitos/Documents/Projects/git/NLPInformationExtraction/src/samplePDFBox/";
 			boolean success = false;
-
 			// Create a new directory, if it doesn't exist
 			File directory = new File(dir);
 			if(directory.exists()) {
@@ -25,7 +26,6 @@ public class writeToNewFile {
 					System.out.printf("Failed to create new directory: %s%n", dir);
 				}
 			}
-
 			// Create a new file, if it does not exist
 			String filename = "temp.txt";
 			File file = new File(filename);
@@ -35,11 +35,15 @@ public class writeToNewFile {
 				System.out.println("No such file exists, creating now.");
 
 				success = file.createNewFile();
+				BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+				writer.write("This is a sample writing");
+				
 				if(success) {
 					System.out.printf("Successfully created new file: %s%n", file);
 				}else {
 					System.out.printf("Failed to create new file: %s%n", file);
 				}
+				writer.close();
 			}
 		}catch(IOException e) {
 			System.out.println("Error: Failed to write new file.");
