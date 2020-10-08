d8091cd00fc86d13bc80fab284d8fc44cf073d96
carrliitos <salazar.ten80@gmail.com>
2020-10-08 13:00:27 -0500

    updates

--- src/Corporas/deleteEmptyRows.py
+++ src/Corporas/deleteEmptyRows.py
@@ -6,8 +6,8 @@ inputCSV = "corpora2.csv"
 outputCSV = "corporaNER3.csv"
 
 with open(inputCSV) as in_file:
-    with open(outputCSV, 'w') as out_file:
-        writer = csv.writer(out_file)
-        for row in csv.reader(in_file):
-            if any(row):
-                writer.writerow(row)
+	with open(outputCSV, 'w') as out_file:
+		writer = csv.writer(out_file)
+		for row in csv.reader(in_file):
+			if any(row):
+				writer.writerow(row)
