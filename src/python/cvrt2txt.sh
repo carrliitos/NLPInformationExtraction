63f529390463e7661ef3825e6d044c7bc43249cf
carrliitos <salazar.ten80@gmail.com>
2020-10-13 20:40:50 -0500

    added convert.py

--- src/python/cvrt2txt.sh
+++ src/python/cvrt2txt.sh
@@ -6,7 +6,7 @@
 for file in $1*.pdf; do
 	echo "Extracting texts from: "
 	echo "$file"
-	pdf2txt.py "$file" > "${file::-4}.txt"
+	python "convert.py" "$file" > "${file::-4}.txt"
 	echo "New file is saved to: "
 	echo "${file::-4}.txt"
 	echo ""
