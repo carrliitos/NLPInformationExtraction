e81e1b5b134d87d52256344e9461bfe54cf9f56e
carrliitos <salazar.ten80@gmail.com>
2020-10-16 12:21:59 -0500

    creating the automation pipeline

--- src/python/cvrt2txt.sh
+++ src/python/cvrt2txt.sh
@@ -7,7 +7,9 @@ for file in $1*.pdf; do
 	echo "Extracting texts from: "
 	echo "$file"
 	python "convert.py" "$file" > "${file::-4}.txt"
-	echo "New file is saved to: "
-	echo "${file::-4}.txt"
-	echo ""
+	mkdir "./output/"
+	mv "${file::-4}.txt" "output/"
 done
+
+echo ""
+echo "Outputs are located in the output/ folder"
